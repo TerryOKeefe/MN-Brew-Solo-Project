966e89eb8d1af364f6c64cd67ca8d768f90f624a
terryokeefe <terryokeefe@users.noreply.github.com>
2021-06-29 17:31:46 -0500

    created add recipe saga

---
+++ src/redux/sagas/add.saga.js
@@ -0,0 +1,24 @@
+import axios from 'axios';
+import { put, takeLatest } from "redux-saga/effects";
+
+// worker saga for 'ADD_RECIPE'
+function* addRecipe(action) {
+    // console log to see response
+    console.log('addRecipe response:', action.payload);
+    try {
+        // post new recipe
+        yield axios.post('/api/recipes', action.payload);
+        // get the recipes to render to DOM
+        yield put({type: 'FETCH_RECIPES'});
+    } catch {
+        // console log to show error location
+        console.log('Error in addRecipe saga');
+    }
+} // end addRecipe
+
+function* addRecipeSaga() {
+    yield takeLatest('ADD_RECIPE', addRecipe)
+} // end addRecipeSaga
+
+// export addRecipeSaga
+export default addRecipeSaga;
