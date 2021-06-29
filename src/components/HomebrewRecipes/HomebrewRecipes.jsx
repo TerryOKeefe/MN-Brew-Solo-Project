29a6b522115287bbe3bfc18dd8f50f83b68420ac
terryokeefe <terryokeefe@users.noreply.github.com>
2021-06-29 11:39:47 -0500

    setup dispatch in recipes page

--- src/components/HomebrewRecipes/HomebrewRecipes.jsx
+++ src/components/HomebrewRecipes/HomebrewRecipes.jsx
@@ -1,6 +1,17 @@
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 
 // function for HomebrewRecipes to display cards
 function HomebrewRecipes() {
+
+    // set dispatch variable
+    const dispatch = useDispatch();
+
+    // load once and dispatch fetch_recipes
+    useEffect(() => {
+        dispatch({ type: 'FETCH_RECIPES' });
+    }, [];)
+
     return (
         <div>
             <h1>Select A Brew To Make</h1>
