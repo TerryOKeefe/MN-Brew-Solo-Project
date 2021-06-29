f4b516e9aab64da690285d8d02981501a18331d4
terryokeefe <terryokeefe@users.noreply.github.com>
2021-06-29 14:49:29 -0500

    setup details redux store

--- src/components/RecipeDetails/RecipeDetails.jsx
+++ src/components/RecipeDetails/RecipeDetails.jsx
@@ -1,8 +1,19 @@
+import { useSelector } from 'react-redux';
+
 // function for recipe details page
 function RecipeDetails() {
+    
+    // get details from the redux store 
+    const details = useSelector(store => store.details)
+    // console log to see data from redux store
+    console.log('Details:', details);
+
     return (
         <div>
             <h1>Recipe Details</h1>
+            <div>
+                {details[0].name}
+            </div>
         </div>
     )
 } // end RecipeDetails
