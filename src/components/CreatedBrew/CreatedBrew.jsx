78daa6b5313a7fb7c234ab6154111c4826bcae10
terryokeefe <terryokeefe@users.noreply.github.com>
2021-07-07 09:23:46 -0500

    display created brew with material-ui accordion

--- src/components/CreatedBrew/CreatedBrew.jsx
+++ src/components/CreatedBrew/CreatedBrew.jsx
@@ -1,8 +1,35 @@
 import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import HopsLogo from '../HopsLogo/HopsLogo';
+import { makeStyles } from '@material-ui/core/styles';
+import Accordion from '@material-ui/core/Accordion';
+import AccordionSummary from '@material-ui/core/AccordionSummary';
+import AccordionDetails from '@material-ui/core/AccordionDetails';
+import Typography from '@material-ui/core/Typography';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+
+// setup material-ui style
+const useStyles = makeStyles((theme) => ({
+    root: {
+        width: '100%',
+    },
+    heading: {
+        fontSize: theme.typography.pxToRem(15),
+        flexBasis: '33.33%',
+        flexShrink: 0,
+    },
+    secondaryHeading: {
+        fontSize: theme.typography.pxToRem(15),
+        color: theme.palette.text.secondary,
+        flexBasis: '33.33%',
+        flexShrink: 0,
+    },
+}));
 
 function CreatedBrew() {
+
+    // import classes/styles from material-ui
+    const classes = useStyles();
 
     // set variable for useDispatch
     const dispatch = useDispatch();
@@ -16,7 +43,7 @@ function CreatedBrew() {
 
     return (
         <div>
-            <div className="component-head"> 
+            <div className="component-head">
                 <h1>Created Brew</h1>
             </div>
             <div className="hop-logo">
@@ -25,8 +52,27 @@ function CreatedBrew() {
             <div>
                 {created.map((create) => {
                     return (
-                        <div>
-                            {create.name} | {create.style} | {create.date}
+                        <div className={classes.root}>
+                            <Accordion>
+                                <AccordionSummary
+                                    expandIcon={<ExpandMoreIcon />}
+                                    aria-controls="panel1a-content"
+                                    id="panel1a-header"
+                                >
+                                    <Typography
+                                        className={classes.heading}
+                                    >
+                                        {create.name}
+                                    </Typography>
+                                    <Typography className={classes.secondaryHeading}>{create.style}</Typography>
+                                    <Typography className={classes.secondaryHeading}>{create.date}</Typography>
+                                </AccordionSummary>
+                                <AccordionDetails>
+                                    <Typography>
+                                        {create.notes}
+                                    </Typography>
+                                </AccordionDetails>
+                            </Accordion>
                         </div>
                     )
                 })}
--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -1,6 +1,5 @@
 import './Dashboard.css';
 import { Link } from 'react-router-dom';
-import ProtectedRoute from '../ProtectedRoute/ProtectedRoute';
 import { useSelector } from 'react-redux';
 import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
