6656ac2293b4ca1792af020be1a291260cc90973
terryokeefe <terryokeefe@users.noreply.github.com>
2021-07-07 10:37:34 -0500

    basic pop up for invite button click

--- src/components/Email/Email.js
+++ src/components/Email/Email.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
 import Dialog from '@material-ui/core/Dialog';
@@ -8,12 +8,46 @@ import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
 
 function Email() {
+    // set local state for dialog action
+    const [open, setOpen] = useState(false);
+
+    const handleClickOpen = () => {
+        setOpen(true);
+    };
+
+    const handleClose = () => {
+        setOpen(false);
+    };
 
     return (
         <div>
-            <div>
-                <button>Invite Friend</button>
-            </div>
+            <Button variant="outlined" color="primary" onClick={handleClickOpen}>
+                Invite Friend
+            </Button>
+            <Dialog open={open} onClose={handleClose} aria-labelledby="form-dialog-title">
+                <DialogTitle id="form-dialog-title">Invite</DialogTitle>
+                <DialogContent>
+                    <DialogContentText>
+                        Invite a friend that you think would enjoy this application.
+                    </DialogContentText>
+                    <TextField
+                        autoFocus
+                        margin="dense"
+                        id="name"
+                        label="Email Address"
+                        type="email"
+                        fullWidth
+                    />
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={handleClose} color="primary">
+                        Cancel
+                    </Button>
+                    <Button onClick={handleClose} color="primary">
+                        Send Email
+                    </Button>
+                </DialogActions>
+            </Dialog>
         </div>
     )
 } // end Email
